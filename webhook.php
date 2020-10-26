39018b40d11d102507e3e20e1487a2edb116f21b
MelquisD <melquisferrer@gmail.com>
2020-10-26 04:40:59 -0300

    as

--- webhook.php
+++ webhook.php
@@ -4,6 +4,9 @@ $fp = fopen("/app/webhook.txt",'wb');
 if (false === $fp) {
     throw new RuntimeException('Unable to open log file for writing');
 }
+else {
+    throw new RuntimeException('open log file for writing');
+}
 fwrite($fp,"Hola");
 fclose($fp);
 echo json_encode(file_exists("/app/webhook.txt"));
