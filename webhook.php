609226a4d1081230d76ac2c62183ba051853e5d5
MelquisD <melquisferrer@gmail.com>
2020-10-26 14:11:35 -0300

    asdasd

--- webhook.php
+++ webhook.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-session_id("cu31f7vk92npq69diq33pv5gqrulgfvo");
+
 $filename = "test";
 //$_SESSION['filename'] = json_encode($_POST);
-echo session_id() ;
+echo session_id("cu31f7vk92npq69diq33pv5gqrulgfvo");
 echo $_SESSION['filename'] ;
 http_response_code(200);
 
