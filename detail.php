ac2b36639f8b0069d6106f177fd4ccb71529d935
MelquisD <melquisferrer@gmail.com>
2020-10-26 01:58:37 -0300

    payer

--- detail.php
+++ detail.php
@@ -44,8 +44,8 @@ $preference->payment_methods = array(
   );
 $preference->items = array($item);
 $preference->external_reference = "melquisferrer@gmail.com";
-//$preference->payer = $payer;
-$preference->auto_return = "approved";
+$preference->payer = $payer;
+//$preference->auto_return = "approved";
 $preference->save();
 ?>
 
