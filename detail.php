105843a7e2b33269b01590148e18a65569001c2b
MelquisD <melquisferrer@gmail.com>
2020-10-26 01:07:55 -0300

    url

--- detail.php
+++ detail.php
@@ -154,7 +154,7 @@ $preference->save();
                                             <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
                                                 <div class="as-tilegallery-element as-image-selected">
                                                     <div class=""></div>
-                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img']  ?>) 2x);">
                                                 </div>
                                                 
                                             </div>
@@ -178,6 +178,7 @@ $preference->save();
                                             </h3>
                                         </div>
                                         <h3 >
+                                        <?php echo $_SERVER['HTTP_HOST'] ?>
                                             <?php echo $_POST['price'] ?>
                                         </h3>
                                         <h3 >
