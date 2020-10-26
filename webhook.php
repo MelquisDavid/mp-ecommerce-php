977b0a899da00d50470e2f4c1441e3767813aa25
MelquisD <melquisferrer@gmail.com>
2020-10-26 04:27:56 -0300

    as

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@
 $fp = fopen("/webhook.txt",'w+');
 fwrite($fp,"Hola");
 fclose($fp);
-echo file_exists("/webhook.txt");
-http_response_code(201);
+echo json_encode(file_exists("/webhook.txt"));
+http_response_code(200);
 
 ?>
