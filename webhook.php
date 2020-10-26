99e7eef7991bcd0b1a2565ff643fb873efde1752
MelquisD <melquisferrer@gmail.com>
2020-10-26 13:32:20 -0300

    session

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 if(!session_id()) session_start();
 $filename = "test";
 $_SESSION['filename'] = json_encode($_POST);
-echo "<script>console.log('Debug Objects: " . $filename . "' );</script>";
+echo $_SESSION['filename'] ;
 http_response_code(200);
 
 ?>
