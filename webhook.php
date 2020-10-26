52f3af579698f6f3f99c6775efc4e2d7d07b2905
MelquisD <melquisferrer@gmail.com>
2020-10-26 14:34:02 -0300

    asdasdasdas

--- webhook.php
+++ webhook.php
@@ -5,8 +5,13 @@ $filename = "test";
 //
 echo session_id("cu31f7vk92npq69diq33pv5gqrulgfvo");
 session_start();
-$_SESSION['filename'] = $_POST["id"];
-echo $_SESSION['filename'] ;
+
+//echo $_SESSION['filename'] ;
+header('Content-Type: application/json');
+$request = file_get_contents('php://input');
+$_SESSION['filename'] = $request;
+$req_dump = print_r( $request, true );
+$fp = file_put_contents( '/app/request.log', $req_dump );
 http_response_code(200);
 
 ?>
