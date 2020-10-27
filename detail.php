96a1b41d010274025722c9b368a2ff025dbd8a0c
MelquisD <melquisferrer@gmail.com>
2020-10-27 14:34:18 -0300

    asdasd

--- detail.php
+++ detail.php
@@ -12,8 +12,7 @@ $link = "https://".$_SERVER['HTTP_HOST'];
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-var_dump("Hola");
-die();
+
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
