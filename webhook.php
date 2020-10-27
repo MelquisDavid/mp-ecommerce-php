0716d45deb5e0713068ab91d3b64c3610b401842
MelquisD <melquisferrer@gmail.com>
2020-10-27 13:01:08 -0300

    asdasdasd

--- webhook.php
+++ webhook.php
@@ -10,7 +10,7 @@ session_start();
 header('Content-Type: application/json');
 $request = file_get_contents('php://input');
 $req_dump = print_r( $request, true );
-$fp = file_put_contents( 'request.log', $req_dump );
+$fp = file_put_contents( '/app/request.log', $req_dump );
 $_SESSION['filename'] = $req_dump;
 http_response_code(200);
 
