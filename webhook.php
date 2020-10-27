98be846e14b8cc613e779225fb635d87cc341f3b
MelquisD <melquisferrer@gmail.com>
2020-10-27 14:06:25 -0300

    asdasdas

--- webhook.php
+++ webhook.php
@@ -5,13 +5,15 @@ $filename = "test";
 //
 echo session_id("cu31f7vk92npq69diq33pv5gqrulgfvo");
 session_start();
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Content-Type: application/json');
+    $request = file_get_contents('php://input');
+    $req_dump = print_r( $request, true );
+    $fp = file_put_contents( '/app/request.log', $req_dump );
+    $_SESSION['filename'] = $req_dump;
+    http_response_code(200);  // The request is using the POST method
+}
 //echo $_SESSION['filename'] ;
-header('Content-Type: application/json');
-$request = file_get_contents('php://input');
-$req_dump = print_r( $request, true );
-$fp = file_put_contents( '/app/request.log', $req_dump );
-$_SESSION['filename'] = $req_dump;
-http_response_code(200);
+
 
 ?>
